71ffb55b0c9f2ccab7db9339692a3baa58f63467
kliment-olechnovic <kliment.olechnovic@gmail.com>
2022-01-06 11:20:04 +0200

    Improved monomer preparation.

--- test/workflow.bash
+++ test/workflow.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-./ftdmp-prepare-monomer -i ./test/reference_dimer/6V3P_AB.pdb -o ./test/monomers/6V3P_A.pdb --restrict-input '[-chain A]' \
+./ftdmp-prepare-monomer -i ./test/reference_dimer/6V3P_AB.pdb -o ./test/monomers/6V3P_A --restrict-input '[-chain A]' \
   --randomize --voronota-js-path ~/git/voronota/expansion_js \
   --prepare-for-relax --conda-path ~/anaconda3 --conda-env alphafold2
 
-./ftdmp-prepare-monomer -i ./test/reference_dimer/6V3P_AB.pdb -o ./test/monomers/6V3P_B.pdb --restrict-input '[-chain B]' \
+./ftdmp-prepare-monomer -i ./test/reference_dimer/6V3P_AB.pdb -o ./test/monomers/6V3P_B --restrict-input '[-chain B]' \
   --randomize --voronota-js-path ~/git/voronota/expansion_js \
   --prepare-for-relax --conda-path ~/anaconda3 --conda-env alphafold2
 
