68c36760d81a3a729f666fed134ac57cb4450448
kliment-olechnovic <kliment.olechnovic@gmail.com>
2022-01-07 15:15:53 +0200

    Moved test output.

--- test/workflow.bash
+++ test/workflow.bash
@@ -1,42 +1,39 @@
 #!/bin/bash
 
-rm -rf ./test/monomers
+export PATH="$HOME/git/voronota/expansion_js:$PATH"
 
-./ftdmp-prepare-monomer -i ./test/reference_dimer/6V3P_AB.pdb -o ./test/monomers/6V3P_A.pdb --restrict-input '[-chain A]' \
-  --randomize --voronota-js-path ~/git/voronota/expansion_js \
-  --prepare-for-relax --conda-path ~/anaconda3 --conda-env alphafold2
+rm -rf ./test/output
 
-./ftdmp-prepare-monomer -i ./test/reference_dimer/6V3P_AB.pdb -o ./test/monomers/6V3P_B.pdb --restrict-input '[-chain B]' \
-  --randomize --voronota-js-path ~/git/voronota/expansion_js \
-  --prepare-for-relax --conda-path ~/anaconda3 --conda-env alphafold2
+./ftdmp-prepare-monomer -i ./test/reference_dimer/6V3P_AB.pdb -o ./test/output/monomers/6V3P_A.pdb --restrict-input '[-chain A]' \
+  --randomize --prepare-for-relax --conda-path ~/anaconda3 --conda-env alphafold2
+
+./ftdmp-prepare-monomer -i ./test/reference_dimer/6V3P_AB.pdb -o ./test/output/monomers/6V3P_B.pdb --restrict-input '[-chain B]' \
+  --randomize --prepare-for-relax --conda-path ~/anaconda3 --conda-env alphafold2
   
-rm -rf ./test/docking_results
+rm -rf ./test/output/docking_results
 
 ./ftdmp-dock \
-  -m1 ./test/monomers/6V3P_A.pdb \
-  -m2 ./test/monomers/6V3P_B.pdb \
-  --logs-output ./test/docking_results \
-  --voronota-js-path ~/git/voronota/expansion_js \
+  -m1 ./test/output/monomers/6V3P_A.pdb \
+  -m2 ./test/output/monomers/6V3P_B.pdb \
+  --logs-output ./test/output/docking_results \
   --parallel-parts 16 \
 | column -t \
-> ./test/docking_table.txt
+> ./test/output/docking_table.txt
 
-cat ./test/docking_table.txt \
+cat ./test/output/docking_table.txt \
 | ./ftdmp-score \
-  -m1 ./test/monomers/6V3P_A.pdb \
-  -m2 ./test/monomers/6V3P_B.pdb \
+  -m1 ./test/output/monomers/6V3P_A.pdb \
+  -m2 ./test/output/monomers/6V3P_B.pdb \
   --parallel-parts 16 \
-  --voronota-js-path ~/git/voronota/expansion_js \
 | column -t \
-> ./test/scoring_table.txt
+> ./test/output/scoring_table.txt
 
 rm -rf ./test/complexes
 
-join <(sort -k 1b,1 ./test/docking_table.txt) <(head -6 ./test/scoring_table.txt | sort -k 1b,1) \
+join <(sort -k 1b,1 ./test/output/docking_table.txt) <(head -6 ./test/output/scoring_table.txt | sort -k 1b,1) \
 | ./ftdmp-build-complex \
-  -m1 ./test/monomers/6V3P_A.pdb \
-  -m2 ./test/monomers/6V3P_B.pdb \
-  -o ./test/complexes/ \
-  --voronota-js-path ~/git/voronota/expansion_js
+  -m1 ./test/output/monomers/6V3P_A.pdb \
+  -m2 ./test/output/monomers/6V3P_B.pdb \
+  -o ./test/complexes/
 
 
