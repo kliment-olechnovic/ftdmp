450d263b553c8cf872342a0a576f500718dce189
kliment-olechnovic <kliment.olechnovic@gmail.com>
2022-01-07 19:04:24 +0200

    Improved testing workflow.

--- test/workflow.bash
+++ test/workflow.bash
@@ -8,10 +8,10 @@ echo "Preparing monomers"
 
 time -p ( \
 ./ftdmp-prepare-monomer -i ./test/reference_dimer/6V3P_AB.pdb -o ./test/output/monomers/6V3P_A.pdb --restrict-input '[-chain A]' \
-  --randomize --random-seed 999 --prepare-for-relax --conda-path ~/anaconda3 --conda-env alphafold2 \
+  --randomize --random-seed 1 --prepare-for-relax --conda-path ~/anaconda3 --conda-env alphafold2 \
 ; \
 ./ftdmp-prepare-monomer -i ./test/reference_dimer/6V3P_AB.pdb -o ./test/output/monomers/6V3P_B.pdb --restrict-input '[-chain B]' \
-  --randomize --random-seed 999 --prepare-for-relax --conda-path ~/anaconda3 --conda-env alphafold2 \
+  --randomize --random-seed 2 --prepare-for-relax --conda-path ~/anaconda3 --conda-env alphafold2 \
 )
 
 
@@ -23,23 +23,23 @@ time -p (./ftdmp-dock \
   --logs-output ./test/output/docking_results \
   --parallel-parts 16 \
 | column -t \
-> ./test/output/docking_table.txt)
+> ./test/output/all_docking_results_table.txt)
 
 
 echo "Scoring in default mode"
 
-time -p (cat ./test/output/docking_table.txt \
+time -p (cat ./test/output/all_docking_results_table.txt \
 | ./ftdmp-score \
   -m1 ./test/output/monomers/6V3P_A.pdb \
   -m2 ./test/output/monomers/6V3P_B.pdb \
   --parallel-parts 16 \
   --colnames-prefix DM_ \
 | column -t \
-> ./test/output/scoring_table.txt)
+> ./test/output/all_scoring_results_table.txt)
 
 echo "Scoring in blanket mode"
 
-time -p (cat ./test/output/docking_table.txt \
+time -p (cat ./test/output/all_docking_results_table.txt \
 | ./ftdmp-score \
   -m1 ./test/output/monomers/6V3P_A.pdb \
   -m2 ./test/output/monomers/6V3P_B.pdb \
@@ -47,29 +47,29 @@ time -p (cat ./test/output/docking_table.txt \
   --colnames-prefix BM_ \
   --parameters '--blanket' \
 | column -t \
-> ./test/output/scoring_blanket_table.txt)
+> ./test/output/all_scoring_blanket_results_table.txt)
 
 
 echo "Joining tables"
 
 time -p (./ftdmp-join-tables \
-  ./test/output/docking_table.txt \
-  ./test/output/scoring_table.txt \
-  ./test/output/scoring_blanket_table.txt \
+  ./test/output/all_docking_results_table.txt \
+  ./test/output/all_scoring_results_table.txt \
+  ./test/output/all_scoring_blanket_results_table.txt \
 | column -t \
-> ./test/output/joined_results_table.txt)
+> ./test/output/all_joined_results_table.txt)
 
 
 echo "Sorting joined table"
 
-time -p (cat ./test/output/joined_results_table.txt \
+time -p (cat ./test/output/all_joined_results_table.txt \
 | ./ftdmp-sort-table \
   --columns "-DM_iface_energy -DM_iface_clash_score -BM_iface_energy" \
   --tolerances "0 0.07 30" \
 | column -t \
-> ./test/output/sorted_joined_results_table.txt)
+> ./test/output/all_sorted_joined_results_table.txt)
 
-cat ./test/output/sorted_joined_results_table.txt \
+cat ./test/output/all_sorted_joined_results_table.txt \
 | head -101 \
 > ./test/output/top_sorted_joined_results_table.txt
 
