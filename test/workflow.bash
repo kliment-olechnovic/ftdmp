22c0f46f0e0c0a99f7239c6cdd30d323375410c1
kliment-olechnovic <kliment.olechnovic@gmail.com>
2022-01-07 15:31:06 +0200

    Emproved testing workflow.

--- test/workflow.bash
+++ test/workflow.bash
@@ -10,8 +10,6 @@ rm -rf ./test/output
 ./ftdmp-prepare-monomer -i ./test/reference_dimer/6V3P_AB.pdb -o ./test/output/monomers/6V3P_B.pdb --restrict-input '[-chain B]' \
   --randomize --prepare-for-relax --conda-path ~/anaconda3 --conda-env alphafold2
   
-rm -rf ./test/output/docking_results
-
 ./ftdmp-dock \
   -m1 ./test/output/monomers/6V3P_A.pdb \
   -m2 ./test/output/monomers/6V3P_B.pdb \
@@ -28,12 +26,10 @@ cat ./test/output/docking_table.txt \
 | column -t \
 > ./test/output/scoring_table.txt
 
-rm -rf ./test/complexes
-
 join <(sort -k 1b,1 ./test/output/docking_table.txt) <(head -6 ./test/output/scoring_table.txt | sort -k 1b,1) \
 | ./ftdmp-build-complex \
   -m1 ./test/output/monomers/6V3P_A.pdb \
   -m2 ./test/output/monomers/6V3P_B.pdb \
-  -o ./test/complexes/
+  -o ./test/output/complexes/
 
 
