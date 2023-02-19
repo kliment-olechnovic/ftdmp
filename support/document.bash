832ab804273eedf23082b03f30ae73760ef6969d
kliment-olechnovic <kliment.olechnovic@gmail.com>
2023-02-19 20:56:05 +0200

    Added html index page.

---
+++ support/document.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd $(dirname "$0")/..
+
+TMPLDIR=$(mktemp -d)
+trap "rm -r $TMPLDIR" EXIT
+
+cat README.md > $TMPLDIR/documentation.markdown
+
+cat > $TMPLDIR/include_in_header.html << 'EOF'
+<style type="text/css">
+a { color: #0000CC; }
+td { padding-right: 1em; }
+div#TOC > ul > li > ul > li ul { display: none; }
+</style>
+EOF
+
+echo "<h1>FTDMP version 1.0</h1>" > $TMPLDIR/include_before_body.html
+
+pandoc $TMPLDIR/documentation.markdown -f markdown -t html --toc -H $TMPLDIR/include_in_header.html -B $TMPLDIR/include_before_body.html -s -o ./index.html
+
